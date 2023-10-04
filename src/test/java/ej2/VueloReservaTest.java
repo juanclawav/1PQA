aa442b4922d37f35a78978ba1853b462b529728f
Juan Cla C <juanclaudiocarrascotapia@gmail.com>
2023-10-04 19:30:52 -0400

    Parcial1 - CommitFinal

--- src/test/java/ej2/VueloReservaTest.java
+++ src/test/java/ej2/VueloReservaTest.java
@@ -6,85 +6,57 @@ import org.mockito.Mockito;
 
 
 public class VueloReservaTest {
-
        TicketChecker ticketCheckerMock = Mockito.mock(TicketChecker.class);
-
         @Test
         public void verifyReservaDeVuelosConPasajesDispo(){
-            Mockito.when(ticketCheckerMock.existenPasajes("La Paz", 2)).thenReturn(true);
-            Mockito.when(ticketCheckerMock.getDay(29, 5, 2023)).thenReturn("Lunes");
+            Mockito.when(ticketCheckerMock.existenPasajes("Sucre", 2)).thenReturn(true);
+            Mockito.when(ticketCheckerMock.getDay(29, 5, 2023)).thenReturn("Domingo");
 
             VueloReserva vr = new VueloReserva();
             vr.setTicketChecker(ticketCheckerMock);
 
-            //output: "el dia Lunes 29 Mayo 2023 existen
-            String actualResult = vr.reservar("La Paz", 2, 29, 5, 2023);
-            String expectedResult = "el dia Lunes 29 Mayo 2023 existen 2 pasajes para La Paz";
+            String actualResult = vr.reservar("Sucre", 3, 28, 4, 2024);
+            String expectedResult = "el dia Domingo 28 Abril 2024 existen 2 pasajes para Sucre";
 
             Assertions.assertEquals(expectedResult, actualResult, "ERROR! La reserva de vuelo es incorrecta.");
 
-            //Verificar que los metodos mockeados se invocaron correctamente
-            Mockito.verify(reservaVuelosHelperMock).existenPasajes("La Paz", 2);
-            Mockito.verify(reservaVuelosHelperMock).getDay(29, 5, 2023);
+            Mockito.verify(ticketCheckerMock).existenPasajes("Sucre", 3);
+            Mockito.verify(ticketCheckerMock).getDay(28, 4, 2024);
         }
 
         @Test
         public void verifyReservaDeVuelosSinPasajesDispo(){
-            //Mockear metodos
-            //ejemplos:
-            //input: La Paz, 10 , 10 , 7 , 2023
-            Mockito.when(reservaVuelosHelperMock.existenPasajes("La Paz", 10)).thenReturn(false);
-            Mockito.when(reservaVuelosHelperMock.getDay(7, 8, 2023)).thenReturn("Lunes");
+            Mockito.when(ticketCheckerMock.existenPasajes("Cochabamba", 1)).thenReturn(false);
+            Mockito.when(ticketCheckerMock.getDay(4, 10, 2023)).thenReturn("Lunes");
 
-            ReservaVuelos reservaVuelos = new ReservaVuelos();
-            reservaVuelos.setReservaVuelosHelper(reservaVuelosHelperMock);
+            VueloReserva reservaVuelos = new VueloReserva();
+            reservaVuelos.setTicketChecker(ticketCheckerMock);
 
-            //output: "no existen suficientes pasajes para La Paz"
-            String actualResult = reservaVuelos.reservaVuelo("La Paz", 10, 10, 7, 2023);
-            String expectedResult = "no existen suficientes pasajes para La Paz";
+            String actualResult = reservaVuelos.reservar("Cochabamba", 5, 21, 9, 2023);
+            String expectedResult = "no existen suficientes pasajes para Cochabamba";
 
             Assertions.assertEquals(expectedResult, actualResult, "ERROR! La reserva de vuelo es incorrecta.");
 
-            //Verificar que los metodos mockeados se invocaron correctamente
-            Mockito.verify(reservaVuelosHelperMock).existenPasajes("La Paz", 10);
+            Mockito.verify(ticketCheckerMock).existenPasajes("La Paz", 5);
         }
 
         @Test
         public void verifyReservaDeVuelosConDiferenteDestino(){
             //Mockear metodos
-            Mockito.when(reservaVuelosHelperMock.existenPasajes("Santa Cruz", 2)).thenReturn(true);
-            Mockito.when(reservaVuelosHelperMock.getDay(17, 3, 2023)).thenReturn("Viernes");
+            Mockito.when(ticketCheckerMock.existenPasajes("Santa Cruz", 2)).thenReturn(true);
+            Mockito.when(ticketCheckerMock.getDay(30, 5, 2023)).thenReturn("Jueves");
 
-            ReservaVuelos reservaVuelos = new ReservaVuelos();
-            reservaVuelos.setReservaVuelosHelper(reservaVuelosHelperMock);
+            VueloReserva vr = new VueloReserva();
+            vr.setTicketChecker(ticketCheckerMock);
 
-            String actualResult = reservaVuelos.reservaVuelo("Santa Cruz", 2, 17, 3, 2023);
-            String expectedResult = "el dia Viernes 17 Marzo 2023 existen 2 pasajes para Santa Cruz";
-
-            Assertions.assertEquals(expectedResult, actualResult, "ERROR! La reserva de vuelo es incorrecta.");
-
-            //Verificar que los metodos mockeados se invocaron correctamente
-            Mockito.verify(reservaVuelosHelperMock).existenPasajes("Santa Cruz", 2);
-            Mockito.verify(reservaVuelosHelperMock).getDay(17, 3, 2023);
-        }
-
-        @Test
-        public void verifyReservaDeVuelosConMesCorrecto(){
-            //Mockear metodos
-            Mockito.when(reservaVuelosHelperMock.existenPasajes("Cochabamba", 3)).thenReturn(true);
-            Mockito.when(reservaVuelosHelperMock.getDay(3, 10, 2023)).thenReturn("Jueves");
-
-            ReservaVuelos reservaVuelos = new ReservaVuelos();
-            reservaVuelos.setReservaVuelosHelper(reservaVuelosHelperMock);
-
-            String actualResult = reservaVuelos.reservaVuelo("Cochabamba", 3, 3, 10, 2023);
-            String expectedResult = "el dia Jueves 3 Octubre 2023 existen 3 pasajes para Cochabamba";
+            String actualResult = vr.reservar("Santa Cruz", 2, 30, 5, 2023);
+            String expectedResult = "el dia Jueves 30 Mayo 2023 existen 2 pasajes para Santa Cruz";
 
             Assertions.assertEquals(expectedResult, actualResult, "ERROR! La reserva de vuelo es incorrecta.");
 
-            //Verificar que los metodos mockeados se invocaron correctamente
-            Mockito.verify(reservaVuelosHelperMock).existenPasajes("Cochabamba", 3);
-            Mockito.verify(reservaVuelosHelperMock).getDay(3, 10, 2023);
+            Mockito.verify(ticketCheckerMock).existenPasajes("Santa Cruz", 2);
+            Mockito.verify(ticketCheckerMock).getDay(30, 5, 2023);
         }
+
     }
-}
+
