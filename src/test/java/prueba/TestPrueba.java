7fe53236b6998826e023a3852b0d3d15440cdf30
enrique <enriqueadhemar@gmail.com>
2023-10-04 08:54:52 -0400

    First commit

---
+++ src/test/java/prueba/TestPrueba.java
@@ -0,0 +1,11 @@
+package prueba;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestPrueba {
+    @Test
+    public void prueba(){
+        Assertions.assertTrue(!false);
+    }
+}
